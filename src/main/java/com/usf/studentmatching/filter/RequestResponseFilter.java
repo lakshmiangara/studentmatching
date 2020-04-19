9127d2dc055a848ec4845444892791363f8cbc67
Lakshmi Angara <angara@mail.usf.edu>
2020-04-19 17:37:13 -0400

    The filter to log incoming and outgoing requests

---
+++ src/main/java/com/usf/studentmatching/filter/RequestResponseFilter.java
@@ -0,0 +1,39 @@
+package com.usf.studentmatching.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
+@Order(2)
+public class RequestResponseFilter implements Filter {
+
+    private final static Logger LOG = LoggerFactory.getLogger(RequestResponseFilter.class);
+    @Override
+    public void init(final FilterConfig filterConfig) throws ServletException {
+        LOG.info("Initializing filter :{}", this);
+    }
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        LOG.info("Logging Request  {} : {}", req.getMethod(), req.getRequestURI());
+        chain.doFilter(request, response);
+        LOG.info("Logging Response :{}", res.getContentType());
+    }
+
+    @Override
+    public void destroy() {
+        LOG.warn("Destructing filter :{}", this);
+    }
+}
