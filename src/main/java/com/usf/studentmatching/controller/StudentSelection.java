351206029265e4ea5352a4ddf370649d45830c70
Lakshmi Angara <angara@mail.usf.edu>
2020-04-19 17:32:14 -0400

    added StudentInterest with additonal service

--- src/main/java/com/usf/studentmatching/controller/StudentSelection.java
+++ src/main/java/com/usf/studentmatching/controller/StudentSelection.java
@@ -8,14 +8,21 @@ import com.usf.studentmatching.repository.StudentInterestRepository;
 import com.usf.studentmatching.utils.MatchingAlgorithm;
 import com.usf.studentmatching.utils.StudentInputValidation;
 import com.usf.studentmatching.utils.StudentPreferences;
-import io.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 @RestController
 public class StudentSelection {
+
+    private final static Logger log = LoggerFactory.getLogger(StudentSelection.class);
 
     @Autowired
     private ProjectRepository projectRepository;
@@ -25,61 +32,70 @@ public class StudentSelection {
 
     @Autowired
     private StudentInputValidation studentInputValidation;
+
      //GET method
     @RequestMapping(method = RequestMethod.GET, value = "/list/project")
     public List<Project> getAllProjects() {
+        log.info("-- getAllProjects --");
         List<Project> projectsList = new ArrayList<Project>();
         projectRepository.findAll().forEach(p -> projectsList.add(p));
         return projectsList;
     }
     //POST method
-    @RequestMapping(method = RequestMethod.POST, value = "/list/studentinterest")
-    public String postStudentInterest(@RequestBody StudentInterest[] studentInterest) {
+    @RequestMapping(method = RequestMethod.POST, value = "/list/studentinterest", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String postStudentInterest(@RequestBody StudentInterest studentInterest) {
+        log.info("-- postStudentInterest --");
         String status = null;
-        for (int i=0; i < studentInterest.length; i++) {
-            if (studentInputValidation.validate(studentInterest[i], projectRepository)) {
-                StudentInterestPrimaryKey studentInterestPrimaryKey =
-                        new StudentInterestPrimaryKey(studentInterest[i].getProjectid(),
-                                studentInterest[i].getStudentid());
-
-                com.usf.studentmatching.model.StudentInterest st =
-                        new com.usf.studentmatching.model.StudentInterest(
-                                studentInterestPrimaryKey,
-                                studentInterest[i].getStudentskills(),
-                                new Date(),
-                                studentInterest[i].getWeekDay(),
-                                studentInterest[i].getFirstName(),
-                                studentInterest[i].getLastName(),
-                                studentInterest[i].getMode(),
-                                studentInterest[i].getPreference()
-                                );
+        if (studentInputValidation.validate(studentInterest, projectRepository)) {
+            StudentInterestPrimaryKey studentInterestPrimaryKey =
+                    new StudentInterestPrimaryKey(studentInterest.getProjectid(),
+                            studentInterest.getStudentid());
+            com.usf.studentmatching.model.StudentInterest st =
+                    new com.usf.studentmatching.model.StudentInterest(
+                            studentInterestPrimaryKey,
+                            studentInterest.getStudentskills(),
+                            new Date(),
+                            studentInterest.getWeekDay(),
+                            studentInterest.getFirstName(),
+                            studentInterest.getLastName(),
+                            studentInterest.getMode(),
+                            studentInterest.getPreference()
+                    );
+            try {
                 studentInterestRepository.save(st);
-                status = "Successful";
-            } else {
-                status = "Please enter all the required values";
+            } catch (Exception e) {
+                status = e.getMessage();
+                log.error("Failed to save {} ", e.getMessage());
+                return status;
             }
+            status = "Saved Successful";
+            log.info("Save is {} ", status);
+        } else {
+            status = "Failed to Save. Please enter all the required values";
+            log.error("Save is {} ", status);
         }
         return status;
     }
 
-
+    //matching algorithm
     @RequestMapping(method = RequestMethod.GET, value="/list/studentMatching")
-    public List<StudentPreferences> getStudentMatching(@RequestParam(required = false) Integer projectId) {
-        if (projectId != null || projectId.intValue() > 0) {
+    public Map<Integer, Set<StudentPreferences>> getStudentMatching(@RequestParam(required = false) Integer projectId) {
+        List<com.usf.studentmatching.model.StudentInterest> stInterest = new ArrayList<>();
+        Map<Integer, Set<StudentPreferences>> studentPreferenceMap =  new HashMap<>();
+        if (projectId != null) {
             Optional<Project> project = projectRepository.findById(projectId);
-            List<com.usf.studentmatching.model.StudentInterest> stInterest = new ArrayList<>();
             studentInterestRepository.findAll().forEach(st -> stInterest.add(st));
-            Map<Integer, List<StudentPreferences>> studentPreferenceMap =  new HashMap<>();
             MatchingAlgorithm.matchPreferences(studentPreferenceMap, stInterest, project.get());
-            return studentPreferenceMap.get(projectId);
+            return studentPreferenceMap;
         }  else {
-            return null;
+           projectRepository.findAll().forEach(project -> {
+               studentInterestRepository.findAll().forEach(st -> stInterest.add(st));
+               MatchingAlgorithm.matchPreferences(studentPreferenceMap, stInterest, project);
+           });
+           return studentPreferenceMap;
         }
     }
 
-    private Boolean validateProjectId(String value) {
-        return !StringUtil.isNullOrEmpty(value);
-    }
 
 
 }
